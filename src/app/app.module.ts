88fdb1d625f93fb9d1fd4685a572a658ac38fc1c
Kaixinguo <kaixinguo@kaixinguo.site>
2019-01-03 02:15:01 +0800

    Add dashboard Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MyMaterialModuleModule } from './my-material-module.module';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
